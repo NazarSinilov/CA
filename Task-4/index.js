6fbbacfef70518b5c01ccce1c7bfd82955a2d89b
Nazar Sinilov <nsinilov.itrum@gmail.com>
2023-03-24 15:48:47 +0300

    complete

--- Task-4/index.js
+++ Task-4/index.js
@@ -1,45 +1,87 @@
-const form = document.querySelector('form');
+const inputAdd = document.querySelector("#inputAdd");
+const buttonAdd = document.querySelector("#buttonAdd");
+const div = document.createElement('div');
+div.style.margin = '10px';
+document.body.appendChild(div);
+let tasks = [];
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  const formData = new FormData(form);
 
-  const fName = formData.get('fName').trim();
-  const lName = formData.get('lName').trim();
-  const email = formData.get('email').trim();
-  const phoneNum = formData.get('phoneNum').trim();
-
-  if (fName && lName && doValidate(email, phoneNum)) {
-    const success = document.createElement('h2');
-    success.innerText = "Данные успешно введены!"
-    success.style.color = 'green';
-    document.body.appendChild(success);
-  } else {
-    const error = document.createElement('h2');
-    error.innerText = "Ошибка заполнения!"
-    error.style.color = 'red';
-    document.body.appendChild(error);
-  }
-
-});
-
-function validateEmail(email) { //Validates the email address
-  var emailRegex = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-  return emailRegex.test(email);
-}
-
-function validatePhone(phone) { //Validates the phone number
-  var phoneRegex = /(\+7|8)[- _]*\(?[- _]*(\d{3}[- _]*\)?([- _]*\d){7}|\d\d[- _]*\d\d[- _]*\)?([- _]*\d){6})/g; // Change this regex based on requirement
-  return phoneRegex.test(phone);
-}
-
-function doValidate(email, phoneNum) {
-  if (!validateEmail(email)){
-    alert("Invalid Email");
-    return false;
-  } else if (!validatePhone(phoneNum)) {
-    alert("Invalid Phone");
-    return false;
-  }
-  return true;
+window.onload = function() {
+  console.log(localStorage.getItem('tasks'));
+  tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+  render();
 };
+
+function render() {
+  div.innerHTML = '';
+  tasks.forEach((el, i) => {
+    const block = document.createElement('div');
+    block.style.margin = '10px';
+    div.appendChild(block);
+
+
+    const text = document.createElement("span");
+    text.innerText = el.title;
+    block.appendChild(text);
+
+    const editButton = document.createElement('button');
+    editButton.innerText = 'Edit';
+    editButton.style.margin = '10px';
+    editButton.addEventListener('click', () => {
+      const saveButton = document.createElement('button');
+      const editInput = document.createElement('input');
+
+      saveButton.innerText = 'Save';
+      editInput.value = el.title;
+
+      saveButton.addEventListener('click', () => {
+        const editTask = {
+          id: el.id,
+          title: editInput.value,
+        };
+        tasks.splice(i, 1, editTask);
+        
+        saveButton.style.display = 'none';
+        editInput.style.display = 'none';
+        text.style.display = 'inline';
+        editButton.style.display = 'inline';
+        deleteButton.style.display = 'inline';
+        render();
+        localStorage.setItem('tasks' , JSON.stringify(tasks));
+      });
+
+      block.prepend(saveButton);
+      block.prepend(editInput);
+
+      text.style.display = 'none';
+      editButton.style.display = 'none';
+      deleteButton.style.display = 'none';
+    });
+    block.appendChild(editButton);
+
+    const deleteButton = document.createElement('button');
+    deleteButton.innerText = 'Delete';
+    deleteButton.addEventListener('click', () => deleteTask(el.id))
+    block.appendChild(deleteButton);
+  });
+
+};
+
+const addTask = () => {
+  tasks.unshift({
+    id: new Date(),
+    title: inputAdd.value,
+  });
+  localStorage.setItem('tasks' , JSON.stringify(tasks));
+  render();
+  inputAdd.value = '';
+};
+
+
+const deleteTask = (id) => {
+  tasks = tasks.filter(el => el.id !== id);
+  localStorage.setItem('tasks' , JSON.stringify(tasks));
+  render();
+}
+
+buttonAdd.addEventListener('click', addTask);
